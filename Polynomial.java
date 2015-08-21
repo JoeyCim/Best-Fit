4d2fbdffdd02a3bea31d682c13a5367bb2b6d4b2
Joey Cimo <jcimocho@asu.edu>
2015-08-20 19:12:18 -0700

    Fixing indentation

--- Polynomial.java
+++ Polynomial.java
@@ -1,35 +1,34 @@
 import java.text.DecimalFormat;
 
-
 public class Polynomial {
-	
-	double[] coefficients;
-	
-	public Polynomial(Matrix coeffMat) {
-		coefficients = new double[coeffMat.getEntries().length];
-		for (int i = 0; i < coefficients.length; i++) {
-			coefficients[i] = coeffMat.getEntries()[i][0];
-		}
-	}
-	
-	//Returns the output for an input 'x'
-	public double evaluate(double x) {
-		double total = 0;
-		
-		for (int i = 0; i < coefficients.length; i++) {
-			total += coefficients[i]*Math.pow(x, i);
-		}
-		return total;
-	}
-	
-	public String toString() {
-		DecimalFormat dF = new DecimalFormat("#.####");
-		String pString = new String();
-		
-		for (int i= coefficients.length - 1; i > 0; i--) {
-			pString += dF.format(coefficients[i]) + "x^" + i + " + ";
-		}
-		pString += dF.format(coefficients[0]) + "";
-		return pString;
-	}
+
+    double[] coefficients;
+
+    public Polynomial(Matrix coeffMat) {
+        coefficients = new double[coeffMat.getEntries().length];
+        for (int i = 0; i < coefficients.length; i++) {
+            coefficients[i] = coeffMat.getEntries()[i][0];
+        }
+    }
+
+    //Returns the output for an input 'x'
+    public double evaluate(double x) {
+        double total = 0;
+
+        for (int i = 0; i < coefficients.length; i++) {
+            total += coefficients[i]*Math.pow(x, i);
+        }
+        return total;
+    }
+
+    public String toString() {
+        DecimalFormat dF = new DecimalFormat("#.####");
+        String pString = new String();
+
+        for (int i= coefficients.length - 1; i > 0; i--) {
+            pString += dF.format(coefficients[i]) + "x^" + i + " + ";
+        }
+        pString += dF.format(coefficients[0]) + "";
+        return pString;
+    }
 }
